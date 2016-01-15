a696f27bc134241fc089689d352037fce353b801
Nagendra <nagendrareddy.d@gmail.com>
2016-01-15 22:48:40 +1100

    Email and SMS API

---
+++ AutogearWeb/Providers/EmailAPI.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net;
+using System.IO;
+using System.Net.Mail;
+
+namespace AutogearWeb.Providers
+{
+    public class EmailAPI
+    {
+        public string SendEmail(List<string> sendEmail,string subject,string body)
+        {
+            try
+            {
+                string emailIds = GetEmailAddress(sendEmail);
+                var fromAddress = new MailAddress("nagenda.au@gmail.com", "Tsting");
+                var toAddress = new MailAddress(emailIds, "To Name");
+                const string fromPassword = "nagendra~1";
+                
+                var smtp = new SmtpClient
+                {
+                    Host = "smtp.gmail.com",
+                    Port = 587,
+                    EnableSsl = true,
+                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                    UseDefaultCredentials = false,
+                    Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
+                };
+                using (var mailMessage = new MailMessage(fromAddress, toAddress)
+                {
+                    Subject = subject,
+                    Body = body
+                })
+                {
+                    smtp.Send(mailMessage);
+                }
+                return "0";
+            }
+            catch(Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        private string GetEmailAddress(List<string> emails)
+        {
+            string emailIds = string.Empty;
+            foreach (string email in emails)
+            {
+                emailIds += ";" + email;
+            }
+            emailIds = emailIds.Remove(0, 1);
+            return emailIds;
+        }
+    }
+}
---
+++ AutogearWeb/Providers/SMSAPI.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net;
+using System.IO;
+
+namespace AutogearWeb.Providers
+{
+    public class SMSAPI
+    {
+        public string SendSMS(string message, List<string> phonenumbers)
+        {
+            try
+            {
+                string numbersList = GetPhoneNumbers(phonenumbers);
+                WebClient client = new WebClient();
+                client.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
+                client.QueryString.Add("username", "testingapp");
+                client.QueryString.Add("password", "Nagendra~1");
+                client.QueryString.Add("to", numbersList);
+                client.QueryString.Add("from", "Autogear");
+                client.QueryString.Add("message", message);
+                client.QueryString.Add("ref", "123");
+                client.QueryString.Add("maxsplit", "2");
+                string baseurl = "http://www.smsbroadcast.com.au/api-adv.php";
+                Stream data = client.OpenRead(baseurl);
+                StreamReader reader = new StreamReader(data);
+                string s = reader.ReadToEnd();
+                string[] response = s.Split(':');
+                data.Close();
+                reader.Close();
+                if (response.Length > 0)
+                    return response[0];
+                else
+                    return "ERROR";
+
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        private string GetPhoneNumbers(List<string> phonenumbers)
+        {
+            string phnNumbers = string.Empty;
+            foreach (string phonenumber in phonenumbers)
+            {
+                phnNumbers += "," + phonenumber;
+            }
+            phnNumbers = phnNumbers.Remove(0, 1);
+            return phnNumbers;
+        }
+    }
+}
