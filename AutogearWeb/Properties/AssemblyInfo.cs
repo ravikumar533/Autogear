14d6bb6f9062b2745364138bbb5e2de62184e5d5
Nagendra <nagendrareddy.d@gmail.com>
2016-01-15 23:17:50 +1100

    added log4net

--- AutogearWeb/Properties/AssemblyInfo.cs
+++ AutogearWeb/Properties/AssemblyInfo.cs
@@ -21,6 +21,7 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("bc7418dd-54b5-4aaa-82e0-3c159ed5a627")]
+[assembly: log4net.Config.XmlConfigurator(ConfigFile = "log4net.config", Watch = true)] 
 
 // Version information for an assembly consists of the following four values:
 //
